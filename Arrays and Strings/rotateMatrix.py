ab91955ffeb8cc140981d29c8873fe255900b35b
kwojewodzki <kejdablju99@gmail.com>
2025-08-06 22:45:56 +0200

    +

---
+++ Arrays and Strings/rotateMatrix.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        self.transpose(matrix)
+        self.reflect(matrix)
+
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i+1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    def reflect(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j], matrix[i][-j-1] = matrix[i][-j-1], matrix[i][j]
+
+matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+
+s = Solution()
+s.rotate(matrix)
+print(matrix)
