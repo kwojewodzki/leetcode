321724673fcff27357d71452a523a653a6e62c8f
kwojewodzki <kejdablju99@gmail.com>
2025-08-06 20:28:53 +0200

    more problems solved

--- Arrays and Strings/3Sum.py
+++ Arrays and Strings/3Sum.py
@@ -2,9 +2,37 @@
 
 Notice that the solution set must not contain duplicate triplets.'''
 
+from typing import List
+
+
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            if i == 0 or nums[i] != nums[i-1]:
+                self.twoSum(nums, i, res)
+        return res
         
+    def twoSum(self, nums, i, res):
+        low = i+1
+        high = len(nums) - 1
+        while low < high:
+            sum= nums[i] + nums[low] + nums[high]
+            if sum < 0:
+                low += 1
+            elif sum > 0:
+                high -= 1
+            else:
+                res.append([nums[i], nums[low], nums[high]])
+                low += 1
+                high -= 1
+                while low < high and nums[low] == nums[low -1]:
+                    low += 1
+
 
 s = Solution()
-print(s.threeSum([-1,0,1,2,-1,-4]))
+print(s.threeSum([-1,0,1,2,-1,-4])),
